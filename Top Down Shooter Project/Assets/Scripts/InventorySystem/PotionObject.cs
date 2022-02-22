56c68e16ac2e36f111ba6268707b247cf30dd553
NomadMoth <mackinsd@lsbu.ac.uk>
2022-02-22 16:39:19 +0000

    Working on inventory system

---
+++ Top Down Shooter Project/Assets/Scripts/InventorySystem/PotionObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Potion Object", menuName = "Inventory System/Items/Potion")]
+
+public class PotionObject : ItemObject
+{
+    public int restoreHealthValue;
+
+    void Awake()
+    {
+        type = ItemType.Food;
+    }
+}
